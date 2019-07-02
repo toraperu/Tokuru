740256ac456bced757874d230e5eb44ca433ce6b
toraperu <toraperu@icloud.com>
2019-07-02 07:23:21 +0000

    退会機能の作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+	#退会してるか否かをチェック
+	before_action :resignation_user?
+	
+	def resignation_user?
+	    if user_signed_in?
+	        if current_user.resignation == true
+	        	sign_out(current_user)
+	      	end
+	    end
+	end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,42 @@
 class UsersController < ApplicationController
 
 	def show
+		@user = User.find(params[:id])
 	end
 
 	def edit
+		@user = User.find(params[:id])
 	end
 
 	def update
+		@user = User.find(params[:id])
+		if @user.update(user_params)
+			sign_in(@user, bypass: true)  #パスワードが変更された時、強制ログアウト=>ログインする
+			flash[:notice] = "編集内容を更新しました"
+			redirect_to user_path(@user.id)
+		else
+			render :"/users/edit"
 	end
+
+	def resign
+		@user = User.find(params[:id])
+	end
+
+	def resign_confirm
+		@user = User.find(params[:id])
+		@user.update(resignation: true)
+		flash[:danger] = "退会しました"
+		redirect_to root_path
+	end
+
+
+	private
+
+	def user_params
+		params.require(:user).permit(
+			:name, :real_name, :profile_image, :email, :password, :password_confirmation)
+	end
+
 
 end
 
