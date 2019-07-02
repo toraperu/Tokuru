f886fb3e45f530ba94018a9e3a51199c505fb742
toraperu <toraperu@icloud.com>
2019-07-02 06:42:58 +0000

    deviseでadminとuser作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+	devise_for :admins, controllers: {
+	  sessions:      'admins/sessions',
+	  passwords:     'admins/passwords',
+	  registrations: 'admins/registrations'
+	}
+	devise_for :users, controllers: {
+	  sessions:      'users/sessions',
+	  passwords:     'users/passwords',
+	  registrations: 'users/registrations'
+	}
+
+#userのルーティング
+	get 'users/:id/resign' => 'users#resign', as: 'resign'
+	patch 'users/:id/resign_confirm' => 'users#resign_confirm', as: 'resign_confirm'
+
+
 end
