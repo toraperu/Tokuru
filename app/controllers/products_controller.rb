31ead74bb2eff84547c41a15300fd34b3a014292
toraperu <toraperu@icloud.com>
2019-07-04 12:01:42 +0000

    fin edit/products edit/user

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -23,16 +23,25 @@ class ProductsController < ApplicationController
   end
 
   def edit
+    @genres = Genre.all
+    @product = Product.find(params[:id])
   end
 
   def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      flash[:notice] = "編集内容を更新しました"
+      redirect_to root_path
+    else
+      render :"/products/edit"
+    end
   end
 
 
   private
 
   def product_params
-    params.require(:product).permit(:name, :body, :jacket_image, :price, :genre_id)
+    params.require(:product).permit(:name, :body, :jacket_image, :price, :genre_id, :finish)
   end
 
 
