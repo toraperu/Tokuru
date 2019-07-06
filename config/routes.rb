3e9e9aa8a8c3aac7a4e6da02aa182e47ecbce08b
toraperu <toraperu@icloud.com>
2019-07-06 07:21:46 +0000

    fin interim show/product

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'comments/index'
+  get 'comments/new'
   get 'genres/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
