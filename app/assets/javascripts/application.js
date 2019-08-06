69e2f903300e551dedd4db8b74ff950511266862
toraperu <toraperu@icloud.com>
2019-08-06 10:30:59 +0000

    商品投稿ページ改善

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,6 +16,23 @@
 
 
 
+//headerの位置
+$(function() {
+  var $win = $(window),
+      $header = $('header'),
+      headerHeight = $header.outerHeight(),
+      startPos = 0;
+
+  $win.on('load scroll', function() {
+    var value = $(this).scrollTop();
+    if ( value > startPos && value > headerHeight ) {
+      $header.css('top', '-' + headerHeight + 'px');
+    } else {
+      $header.css('top', '0');
+    }
+    startPos = value;
+  });
+});
 
 //ページトップに戻るボタン
 $(function() {
