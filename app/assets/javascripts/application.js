5a4c17ff9801a1e900aaa57d8013d389af18f2f4
toraperu <toraperu@icloud.com>
2019-11-05 10:09:02 +0000

    improve jumbotron

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,26 +16,6 @@
 
 
 
-//headerの位置=>固定、下にスクロールすると隠れる
-$(function() {
-  var $win = $(window),
-      $header = $('header'),
-      headerHeight = $header.outerHeight(),
-      startPos = 0;
-
-  $win.on('load scroll', function() {
-    var value = $(this).scrollTop();
-    if ( value > startPos && value > headerHeight ) {
-      $header.css('top', '-' + headerHeight + 'px');
-    } else {
-      $header.css('top', '0');
-    }
-    startPos = value;
-  });
-});
-
-
-
 
 //ページトップに戻るボタン
 $(function() {
